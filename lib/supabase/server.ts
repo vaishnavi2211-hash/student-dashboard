dd325ff1bb813c51d408d9cbb0e380afeff2a67e
vaishu.. <vaishnaviseshapu09@gmail.com>
2026-05-25 22:40:15 +0530

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,9 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
 
-export function createClient() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
